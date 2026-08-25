99c1ef0461df78d5c306d55b4a85e250168645d6
haider-rn <ha903540@gmail.com>
2026-08-25 19:12:10 +0500

    added state for user name and password

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,7 +2,29 @@ import { useState } from "react";
 import "../CSS/LoginPage.css";
 
 function LoginPage() {
+  const [username, setUserName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
   const [showPassword, setShowPassword] = useState(false);
+
+  const handleLogin = () => {
+    if (!username || !password) {
+      return alert("Fill the credentials!");
+    }
+
+    if (
+      username?.toLowerCase() === "admin" &&
+      password?.toLowerCase() === "myuog"
+    ) {
+      const expiry = Date.now() + 7 * 24 * 60 * 60 * 1000;
+      localStorage.setItem("expire_credentials", String(expiry));
+      localStorage.setItem("authStatus", String("true"));
+    } else {
+      alert("Wrong credentials!");
+      setUserName("");
+      setPassword("");
+    }
+  };
 
   return (
     <div className="authContainer">
@@ -23,6 +45,10 @@ function LoginPage() {
           placeholder="Enter your username"
           className="inputField"
           autoComplete="username"
+          value={username}
+          onChange={(e) => {
+            setUserName(e?.target?.value);
+          }}
         />
       </div>
 
@@ -36,19 +62,31 @@ function LoginPage() {
             placeholder="Enter your password"
             className="inputField"
             autoComplete="current-password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e?.target?.value);
+            }}
           />
 
           <button
             type="button"
             className="passwordToggle"
-            onClick={() => setShowPassword(!showPassword)}
+            onClick={() => {
+              setShowPassword(!showPassword);
+            }}
           >
             {showPassword ? "Hide" : "Show"}
           </button>
         </div>
       </div>
 
-      <button type="button" className="login-btn">
+      <button
+        type="button"
+        className="login-btn"
+        onClick={() => {
+          handleLogin();
+        }}
+      >
         Sign in
       </button>
 
