6d3f3f38c0d2d0ef2e9cc2abf054df23b4a8237a
haider-rn <ha903540@gmail.com>
2026-09-21 19:39:57 +0500

    added endpoint for vu study app data

--- src/redux/user/userApi.ts
+++ src/redux/user/userApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { GOOGLE_SHEET_API_URL } from "../../utils/constants";
-import type { UOG_DATA_TYPE } from "./type";
+import type { UOG_DATA_TYPE, VU_STUDY_DATA_TYPE } from "./type";
 
 export const userApi = createApi({
   reducerPath: "userApi",
@@ -10,9 +10,13 @@ export const userApi = createApi({
       query: () =>
         `AKfycbzZkgTYLPD6hZJTa8et6DjumjKbyxS5mr92Mm5SzQC82l9Qyrq1x6s0GbvPs7B7_6yCeQ/exec`,
     }),
+     getMyVUStudyAppData: builder.query<VU_STUDY_DATA_TYPE, void>({
+      query: () =>
+        `AKfycbz-Q59cUDuRRKDABqpRZxZY9w_9CyBMILw07iW7iJRvkSat6TOTNVpjXquku-pq2vXtoA/exec`,
+    }),
   }),
 });
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetMyUogAppDataQuery } = userApi;
+export const { useGetMyUogAppDataQuery, useGetMyVUStudyAppDataQuery } = userApi;
